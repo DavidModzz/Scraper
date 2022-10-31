bf649286ec5564adb78bb2244604fca00cd2e957
David ??? <86541514+DavidModzz@users.noreply.github.com>
2022-10-30 22:43:45 -0300

    Ｍｕｌｔｉ－Ｓｃｒａｐｅｒ

---
+++ index.js
@@ -0,0 +1,17 @@
+const {
+  styletext,
+  pinterest,
+  ringtone,
+  wikimedia,
+  tiktokStalk,
+  igStalk
+  twitter
+} = require("./lib/scraper")
+
+exports.styletext = styletext
+exports.pinterest = pinterest
+exports.ringtone = ringtone
+exports.wikimedia = wikimedia
+exports.tiktokStalk = tiktokStalk
+exports.igStalk = igStalk
+exports.twitter = twitter
